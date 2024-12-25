91deb6833447b16845a671194276ca752d3b081f
Tom Harrison <tomhrr@tomhrr.org>
2024-12-26 08:48:21 +1000

    [main] Restore backspace in raise layer.

--- keymap.c
+++ keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_RAISE] = LAYOUT_ortho_4x12(
-    KC_TAB  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_TILD ,
+    KC_TAB  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSPC ,
     KC_NO   , KC_LT   , KC_GT   , KC_MINS , KC_UNDS , KC_QUOT , KC_DQT  , KC_EQL  , KC_PLUS , KC_LBRC , KC_RBRC , KC_BSLS ,
     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT_ortho_4x12(
     KC_NO   , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_DEL  ,
-    KC_NO   , KC_PIPE , KC_GRV  , KC_NO   , KC_NO   , KC_NO   , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_PGUP , KC_PGDN ,
+    KC_NO   , KC_PIPE , KC_GRV  , KC_TILD , KC_NO   , KC_NO   , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_PGUP , KC_PGDN ,
     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______
   ),
