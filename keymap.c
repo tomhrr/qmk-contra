67badfee5f96286841985c472fb5fb0a34b31f3f
Tom Harrison <tomhrr@tomhrr.org>
2024-12-26 08:45:06 +1000

    [main] Add missing keys, remove shifted behaviour where shifted key is available separately.

--- keymap.c
+++ keymap.c
@@ -38,25 +38,43 @@ enum planck_keycodes {
   EXT_PLV
 };
 
+const key_override_t no_shift_comm_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_COMM);
+const key_override_t no_shift_dot_override  = ko_make_basic(MOD_MASK_SHIFT, KC_DOT,  KC_DOT);
+const key_override_t no_shift_grv_override  = ko_make_basic(MOD_MASK_SHIFT, KC_GRV,  KC_GRV);
+const key_override_t no_shift_mins_override = ko_make_basic(MOD_MASK_SHIFT, KC_MINS, KC_MINS);
+const key_override_t no_shift_quot_override = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, KC_QUOT);
+const key_override_t no_shift_eql_override  = ko_make_basic(MOD_MASK_SHIFT, KC_EQL,  KC_EQL);
+const key_override_t no_shift_bsls_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSLS, KC_BSLS);
+
+const key_override_t *key_overrides[] = {
+    &no_shift_comm_override,
+    &no_shift_dot_override,
+    &no_shift_grv_override,
+    &no_shift_mins_override,
+    &no_shift_quot_override,
+    &no_shift_eql_override,
+    &no_shift_bsls_override
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_ortho_4x12(
     KC_ESC  , KC_Q  , KC_W  , KC_E  , KC_R    , KC_T   , KC_Y  , KC_U    , KC_I    , KC_O   , KC_P     , KC_BSPC ,
-    KC_GRV  , KC_A  , KC_S  , KC_D  , KC_F    , KC_G   , KC_H  , KC_J    , KC_K    , KC_L   , KC_SCLN  , KC_ENT  ,
+    KC_NO   , KC_A  , KC_S  , KC_D  , KC_F    , KC_G   , KC_H  , KC_J    , KC_K    , KC_L   , KC_SCLN  , KC_ENT  ,
     KC_NO   , KC_Z  , KC_X  , KC_C  , KC_V    , KC_B   , KC_N  , KC_M    , KC_COMM , KC_DOT , KC_SLASH , KC_NO   ,
     KC_NO   , KC_NO , KC_NO , LOWER , KC_LSFT , KC_SPC , RAISE , KC_RCTL , KC_RALT , KC_NO  , KC_NO    , KC_NO
   ),
 
-  [_LOWER] = LAYOUT_ortho_4x12(
-    KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_BSPC ,
-    KC_PIPE , KC_LT   , KC_GT   , KC_MINS , KC_UNDS , KC_QUOT , KC_DQT  , KC_EQL  , KC_PLUS , KC_LBRC , KC_RBRC , KC_BSLS ,
+  [_RAISE] = LAYOUT_ortho_4x12(
+    KC_TAB  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_TILD ,
+    KC_NO   , KC_LT   , KC_GT   , KC_MINS , KC_UNDS , KC_QUOT , KC_DQT  , KC_EQL  , KC_PLUS , KC_LBRC , KC_RBRC , KC_BSLS ,
     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______
   ),
 
-  [_RAISE] = LAYOUT_ortho_4x12(
-    KC_TAB  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSPC ,
-    KC_PIPE , KC_LT   , KC_GT   , KC_MINS , KC_UNDS , KC_QUOT , KC_DQT  , KC_EQL  , KC_PLUS , KC_LBRC , KC_RBRC , KC_BSLS ,
+  [_LOWER] = LAYOUT_ortho_4x12(
+    KC_NO   , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_DEL  ,
+    KC_NO   , KC_PIPE , KC_GRV  , KC_NO   , KC_NO   , KC_NO   , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_PGUP , KC_PGDN ,
     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______
   ),
